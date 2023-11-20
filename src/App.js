8a26f2944d4366055221f566a8381a4a714c3470
Arj573 <wijayaarjuna2003@gmail.com>
2023-11-20 21:42:11 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Special from './components/Special'
+import Product from './components/Product'
+
+
+function App() {
+  return (
+  <>
+    <Header />
+    <main className = "main"> 
+    <Hero />
+    <Special />
+    <Product />
+    </main>
+  </>
+  );
+}
+
+export default App;
